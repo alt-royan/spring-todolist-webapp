f1da3bee2e849d90c43525001f7d4bcd95616251
Grigory Legostaev <grig03@bk.ru>
2021-11-05 17:58:08 +0300

    Spring todo-list web app initial commit.

---
+++ src/test/java/todolist/springToDoList/SpringToDoListApplicationTests.java
@@ -0,0 +1,13 @@
+package todolist.springToDoList;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringToDoListApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
